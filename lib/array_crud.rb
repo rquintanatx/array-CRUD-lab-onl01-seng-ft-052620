5102713b93ed953b5175ac198324dc6cea413ec4
Robert Quintana <robertquintanatx@gmail.com>
2020-05-28 01:15:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  emp_array = []
+  []
 end
 
 def create_an_array
-  
+  [5,4,5,6]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
